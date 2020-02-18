8a0a0e99e6d3653b4898bd1aa90bdf8749c53c42
Pedro Henrique Freitas de Paula <phfedev@gmail.com>
2020-02-18 13:52:04 -0300

    Adicionado a rota de cadastro de dados do passageiro

--- src/routes.js
+++ src/routes.js
@@ -38,6 +38,21 @@ routes.post('/sms', (req, res) =>{
 //     });
 // });
 
+routes.post('/novoCadastro', (req, res) =>{
+    var pessoa = req.body
+    console.log(req.body);
+    const client = new totalvoice("f1e7d9db50fa289d2080518116eaad6a");
+    client.sms.enviar(pessoa.numero, "Olá passageiro, recebemos o seu número, assim que soubermos de alguma mudança iremos te informar")
+    .then(function(data) {
+        return res.send(data);
+    })
+    .catch(function(error) {
+        return res.send(error);
+    });
+    setTimeout(() => client.tts.enviar(pessoa.numero, `Olá, o voo ${pessoa.voo} vai atrasar uma hora, qualquer novidade voltamos a te avisar`),30000)
+    return res.send("Recebemos seus dados");
+});
+
 routes.post('/vooatrasado', (req, res) => {
     var pessoa = req.body
     const client = new totalvoice("f1e7d9db50fa289d2080518116eaad6a");
